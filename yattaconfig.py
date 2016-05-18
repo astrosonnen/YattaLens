f0e3ea01640a2fc87313f2178c4dd97427caadc9
sonnen <alessandro.sonnenfeld@ipmu.jp>
2016-05-18 16:46:12 +0900

    added crapstart

--- yattaconfig.py
+++ yattaconfig.py
@@ -1,19 +1,30 @@
 import os
 
 datadir = os.environ.get('YATTA_DATADIR')
-psfdir = os.environ.get('YATTA_PSFDIR')
 logdir = os.environ.get('YATTA_LOGDIR')
 modeldir = os.environ.get('YATTA_MODELDIR')
 
 if datadir is None:
-    datadir = 'cutout_dir/'
-
-if psfdir is None:
-    psfdir = datadir
+    datadir = 'data/'
 
 if logdir is None:
     logdir = 'logs/'
 
 if modeldir is None:
     modeldir = 'models/'
+
+maxarcdist = 30.
+minarcdist = 3.
+minarcsize = 20.
+maxarcsize = 500.
+maxarcdang = 30.
+crapstart = 1.3
+abmin = 1.4
+
+source_range = 8.
+
+fitband = ('g')
+lightband = ('i')
+
+rgbbands = ('i', 'r', 'g')
 

7c8c7891207626c729c830f0c22588d9fabe9322
sonnen <alessandro.sonnenfeld@ipmu.jp>
2016-08-30 22:09:08 +0900

    readded old naming convention

--- master_server_tools/labeling.py
+++ master_server_tools/labeling.py
@@ -1,6 +1,26 @@
 from math import floor
 
 
+def old_coords2name(rr, dd):
+    flag=0
+    rr1=floor(rr/15.)
+    rr2=floor((rr/15.-rr1)*60)
+    rr3=(((rr/15.-rr1)*60) - rr2)*60
+
+    if(dd<0):
+        flag=1
+        dd=-1*dd
+
+    dd1=floor(dd)
+    dd2=floor((dd-dd1)*60)
+    dd3=(((dd-dd1)*60)-dd2)*60
+
+    if(flag):
+        return "%02d%02d%02d-%02d%02d%02d"%(rr1,rr2,round(rr3),dd1,dd2,round(dd3))
+    else:
+        return "%02d%02d%02d+%02d%02d%02d"%(rr1,rr2,round(rr3),dd1,dd2,round(dd3))
+
+
 def coords2name(rr, dd):
     flag=0
     rr1=floor(rr/15.)

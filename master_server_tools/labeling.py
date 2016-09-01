a0963cae47b53e38c9d6f190f823ebb79ad840e3
sonnen <alessandro.sonnenfeld@ipmu.jp>
2016-09-01 14:32:44 +0900

    not sure what's different

--- master_server_tools/labeling.py
+++ master_server_tools/labeling.py
@@ -1,7 +1,7 @@
 from math import floor
 
 
-def coords2name(rr, dd):
+def old_coords2name(rr, dd):
     flag=0
     rr1=floor(rr/15.)
     rr2=floor((rr/15.-rr1)*60)
@@ -19,5 +19,25 @@ def coords2name(rr, dd):
         return "%02d%02d%02d-%02d%02d%02d"%(rr1,rr2,round(rr3),dd1,dd2,round(dd3))
     else:
         return "%02d%02d%02d+%02d%02d%02d"%(rr1,rr2,round(rr3),dd1,dd2,round(dd3))
+
+
+def coords2name(rr, dd):
+    flag=0
+    rr1=floor(rr/15.)
+    rr2=floor((rr/15.-rr1)*60)
+    rr3=(((rr/15.-rr1)*60) - rr2)*60
+
+    if(dd<0):
+        flag=1
+        dd=-1*dd
+
+    dd1=floor(dd)
+    dd2=floor((dd-dd1)*60)
+    dd3=(((dd-dd1)*60)-dd2)*60
+
+    if(flag):
+        return "%02d%02d%02d-%02d%02d%02d"%(rr1,rr2,floor(rr3),dd1,dd2,floor(dd3))
+    else:
+        return "%02d%02d%02d+%02d%02d%02d"%(rr1,rr2,floor(rr3),dd1,dd2,floor(dd3))
 
 

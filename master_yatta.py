1ae90cb9e31e3cc4ffde7d0853edcd4e0a3fbfa2
sonnen <alessandro.sonnenfeld@ipmu.jp>
2017-03-21 18:07:21 +0900

    updated master server version of YattaLens

--- master_yatta.py
+++ master_yatta.py
@@ -1,47 +1,52 @@
-from yattaconfig import *
+import yattaconfig
 import numpy as np
 import sys
 import yattaobjects as yo
-#from pylens import plotting_tools
+from pylens import nopil_plotting_tools
+from master_server_tools import data_tools
 import object_finder_tools as oft
 import pickle
 import yattafitters as fitters
 import time
 import os
-from master_server_tools import data_tools
 
 
 tstart = time.clock()
 
-write_config_file()
+yattaconfig.write_config_file()
+if len(sys.argv) > 1:
+    configname = sys.argv[1]
+else:
+    configname = 'default.yatta'
 
-catname = sys.argv[1]
+config = yattaconfig.read_config_file(configname)
+for par in config:
+    print par, config[par]
 
-sumname = catname.split('/')[-1].split('.')[0] + '.summary'
+allbands = list(config['rgbbands'])
+if not config['fitband'] in allbands:
+    allbands.append(config['fitband'])
+if not config['lightband'] in allbands:
+    allbands.append(config['lightband'])
 
-print 'writing results in file %s'%sumname
+if os.path.isfile(config['catalog_file']):
+    f = open(config['catalog_file'], 'r')
+    input_lines = f.readlines()
+    f.close()
 
-f = open(catname, 'r')
-cand_names = np.loadtxt(f, usecols=(0, ), dtype=str)
-f.close()
-
-f = open(catname, 'r')
-radec = np.loadtxt(f, usecols=(1, 2), dtype=float)
-f.close()
-
-cand_ra = radec[:, 0]
-cand_dec = radec[:, 1]
-
-ncand = len(cand_names)
+else:
+    input_lines = [config['catalog_file']]
 
 summary_lines = []
-summary_lines.append('# Target_name\tra\tdec\tSuccess\tReason\tbestmodel_no\tdata_flag\n')
+#summary_lines.append('# Target_name\tSuccess\tReason\tbestmodel_no\tdata_flag\n')
+for input_line in input_lines:
 
-for n in range(ncand):
+    name = input_line.split()[0]
+    summary_line = input_line[:-1]
 
     data_tools.fetch_data((cand_ra[n], cand_dec[n]), outname=cand_names[n], bands=('g', 'r', 'i'), outdir=datadir)
 
-    cand = yo.Candidate(name=cand_names[n], bands=allbands, ra=cand_ra[n], dec=cand_dec[n])
+    cand = yo.Candidate(name=name, bands=allbands, config=config)
 
     loglines = []
 
@@ -53,33 +58,38 @@ for n in range(ncand):
 
     if cand.read_data():
 
-        if cand.imshape[0] == ny_expected and cand.imshape[1] == nx_expected:
-            data_flag = 0
+        if config['expected_size'] is not None:
+            if cand.imshape[0] == config['expected_size'] and cand.imshape[1] == config['expected_size']:
+                data_flag = 0
+            else:
+                data_flag = 2
         else:
-            data_flag = 2
+            data_flag = 0
 
         light_model = yo.light_model(cand)
 
-        lenspars, junkmask = oft.find_lens(cand, detect_band=lightband, detect_thresh=3.)
+        lenspars, junkmask = oft.find_lens(cand, detect_band=config['lightband'], detect_thresh=3., config=config)
 
-        if lenspars is not None and junkmask[cand.R < lightfitrmax].sum() > 0:
+        if lenspars is not None and junkmask[cand.R < config['lightfitrmax']].sum() > 0:
 
             tlenssub_start = time.clock()
 
             guess = [lenspars['x'], lenspars['y'], lenspars['pa'], 1./lenspars['ab'], lenspars['npix']**0.5/np.pi, 4.]
-            fitters.fit_light(cand, light_model, rmax=lightfitrmax, lfitband=(lightband), mask=junkmask, guess=guess, \
+            fitters.fit_light(cand, light_model, rmax=config['lightfitrmax'], lfitband=config['lightband'], mask=junkmask, guess=guess, \
                               nsamp=50)
 
             tlenssub_end = time.clock()
             loglines.append('QUICK_SUBTRACTION_TIME %f\n'%(tlenssub_end - tlenssub_start))
 
-            objects, arcs, segmap, foundarcs = oft.find_objects(cand, detect_band=fitband, detect_thresh=3.)
+            objects, arcs, segmap, foundarcs = oft.find_objects(cand, detect_band=config['fitband'], detect_thresh=3., \
+                                                                config=config)
             tphase1_end = time.clock()
             loglines.append('PHASE_1_TIME %f\n'%(tphase1_end - tstart))
             loglines.append('ARC_CANDIDATES %d\n'%(len(arcs)))
 
             if foundarcs:
-                iobjs, iarcs, junkmask, ifoundarcs = oft.find_objects(cand, detect_band=lightband, detect_thresh=3.)
+                iobjs, iarcs, junkmask, ifoundarcs = oft.find_objects(cand, detect_band=config['lightband'], detect_thresh=3., \
+                                                                      config=config)
                 junkmask[junkmask > 0] = 1
                 junkmask = 1 - junkmask
                 junkmask[cand.R < 5.] = 1
@@ -87,19 +97,22 @@ for n in range(ncand):
                 print 'arcs found: %d'%(len(arcs))
 
                 guess = [cand.x, cand.y, cand.light_pa, cand.light_q, cand.light_re, cand.light_n]
-                fitters.fit_light(cand, light_model, lfitband=(lightband), mask=junkmask, guess=guess, nsamp=200, \
-                                  rmax=lightfitrmax)
+                fitters.fit_light(cand, light_model, lfitband=config['lightband'], mask=junkmask, guess=guess, nsamp=200, \
+                                  rmax=config['lightfitrmax'])
 
                 foreground_model = yo.foreground_model(cand, iobjs + iarcs, arcs)
 
                 fitters.fit_foregrounds_fixedamps(cand, foreground_model, light_model)
 
-                objects = oft.measure_fluxes(objects, cand, foreground_model, meas_bands=(fitband, lightband))
-                arcs = oft.measure_fluxes(arcs, cand, foreground_model, meas_bands=(fitband, lightband))
+                objects = oft.measure_fluxes(objects, cand, foreground_model, \
+                                             meas_bands=(config['fitband'], config['lightband']))
+
+                arcs = oft.measure_fluxes(arcs, cand, foreground_model, \
+                                          meas_bands=(config['fitband'], config['lightband']))
 
                 nobjs = len(objects)
 
-                cand.image_sets = oft.determine_image_sets(objects, arcs, iobjs + iarcs, band1=fitband, band2=lightband)
+                cand.image_sets = oft.determine_image_sets(objects, arcs, iobjs + iarcs, config=config)
 
                 nsets = len(cand.image_sets)
 
@@ -111,16 +124,16 @@ for n in range(ncand):
 
                 for i in range(nsets):
 
-                    figname = figdir+'/%s_imset%d.png'%(cand.name, i+1)
+                    figname = config['figdir']+'/%s_imset%d.png'%(cand.name, i+1)
 
                     bluest = np.inf
                     for arc in cand.image_sets[i]['arcs']:
-                        ratio = (arc['%s_flux'%lightband] - 3.*arc['%s_err'%lightband])/\
-                                (arc['%s_flux'%fitband] + 3.*arc['%s_err'%fitband])
-                    if ratio < bluest:
-                        bluest = ratio
+                        ratio = (arc['%s_flux'%config['lightband']] - 3.*arc['%s_err'%config['lightband']])/\
+                                (arc['%s_flux'%config['fitband']] + 3.*arc['%s_err'%config['fitband']])
+                        if ratio < bluest:
+                            bluest = ratio
 
-                    if bluest < 10.**(gmi_max/2.5):
+                    if bluest < 10.**(config['color_maxdiff']/2.5):
 
                         print 'set %d: %d arcs, %d images'%(i+1, len(cand.image_sets[i]['arcs']), \
                                                             len(cand.image_sets[i]['images']))
@@ -153,7 +166,7 @@ for n in range(ncand):
 
                         loglines.append('LENS_MODEL_AVG_CHI2 %2.1f'%rchi2)
 
-                        if cand.model_angular_aperture > min_aperture:# and rchi2 < chi2_thresh:
+                        if cand.model_angular_aperture > config['min_aperture']:# and rchi2 < chi2_thresh:
 
                             success = False
 
@@ -181,6 +194,10 @@ for n in range(ncand):
                                 lensness.append((secondbest - cand.lensfit_chi2)/cand.lensfit_chi2)
                                 lenssets.append(i)
 
+                                f = open(config['modeldir']+'/%s_model_set%d.dat'%(cand.name, i+1), 'w')
+                                pickle.dump(cand, f)
+                                f.close()
+
                             else:
                                 if not isalens:
                                     if len(reason) > 0:
@@ -191,12 +208,18 @@ for n in range(ncand):
                                 print 'failed'
                                 cpdir = 'failure'
 
-                            #plotting_tools.make_full_rgb(cand, cand.image_sets[i], outname=figname, success=success)
+                            rgb_arrays = nopil_plotting_tools.make_rgb_arrays(cand, cand.image_sets[i])
+                            f = open(config['figdir']+'/%s_rgbarray_set%d.dat'%(cand.name, i+1), 'w')
+                            pickle.dump(rgb_arrays, f)
+                            f.close()
 
                             #os.system('cp %s %s/'%(figname, cpdir))
 
                         else:
-                            #plotting_tools.make_full_rgb(cand, cand.image_sets[i], outname=figname, success=False)
+                            rgb_arrays = nopil_plotting_tools.make_rgb_arrays(cand, cand.image_sets[i])
+                            f = open(config['figdir']+'/%s_rgbarray_set%d.dat'%(cand.name, i+1), 'w')
+                            pickle.dump(rgb_arrays, f)
+                            f.close()
 
                             if not isalens:
                                 if len(reason) > 0:
@@ -207,25 +230,39 @@ for n in range(ncand):
                         cand.lensfit_model = None
                         cand.lensfit_chi2 = None
 
+                        if config['makeallfigs']:
+                            rgb_arrays = nopil_plotting_tools.make_rgb_arrays(cand, cand.image_sets[i])
+                            f = open(config['figdir']+'/%s_rgbarray_set%d.dat'%(cand.name, i+1), 'w')
+                            pickle.dump(rgb_arrays, f)
+                            f.close()
+
                         if not isalens:
                             if len(reason) > 0:
                                 reason += '_ARC_TOO_RED'
                             else:
                                 reason = 'ARC_TOO_RED'
 
-
-                    f = open(modeldir+'/%s_model_set%d.dat'%(cand.name, i+1), 'w')
-                    pickle.dump(cand, f)
-                    f.close()
+                    if config['saveallmodels']:
+                        f = open(config['modeldir']+'/%s_model_set%d.dat'%(cand.name, i+1), 'w')
+                        pickle.dump(cand, f)
+                        f.close()
 
                 tphase2_end = time.clock()
                 loglines.append('PHASE_2_TIME %f\n'%(tphase2_end - tphase1_end))
 
             else:
+                if config['makeallfigs']:
+                    figname = config['figdir']+'/%s_noarcs.png'%cand.name
+                    image_set = {'junk': [obj for obj in objects], 'foregrounds': [], 'arcs': [], 'images': [], \
+                                 'bad_arcs': []}
+                    plotting_tools.make_full_rgb(cand, image_set=image_set, outname=figname, success=None)
 
                 reason = 'NO_ARCS_FOUND'
 
         else:
+            if config['makeallfigs']:
+                figname = config['figdir']+'/%s_nolens.png'%cand.name
+                plotting_tools.make_full_rgb(cand, image_set=None, outname=figname, success=None)
 
             reason = 'NO_GALAXY_FOUND'
 
@@ -240,18 +277,20 @@ for n in range(ncand):
 
         bset = slenssets[-1]
 
-        summary_line = '%s %f %f YES YATTA %d %d\n'%(cand.name, cand.ra, cand.dec, bset+1, data_flag)
+        summary_line += ' YATTA %d %d\n'%(bset+1, data_flag)
 
     else:
-        summary_line = '%s %f %f NO %s 0 %d\n'%(cand.name, cand.ra, cand.dec, reason, data_flag)
+        summary_line += ' %s 0 %d\n'%(reason, data_flag)
 
-    f = open(sumname, 'a')
+    print 'writing output in file %s'%config['summary_file']
+
+    f = open(config['summary_file'], 'a')
     f.writelines([summary_line])
     f.close()
 
     tend = time.clock()
     loglines.append('TOTAL_TIME %f\n'%(tend - tstart))
-    logfile = open(logdir+cand.name+'.txt', 'w')
+    logfile = open(config['logdir']+name+'.txt', 'w')
     logfile.writelines(loglines)
     logfile.close()
 

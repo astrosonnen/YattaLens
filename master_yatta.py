f0214fb863decdaa4399b6ea85ca109b25562784
astrosonnen <sonnen@physics.ucsb.edu>
2019-02-24 20:57:27 +0100

    moved unnecessary read coordinates

--- master_yatta.py
+++ master_yatta.py
@@ -51,10 +51,10 @@ for input_line in input_lines:
 
         summary_line = input_line[:-1]
 
-        cand_ra = float(input_line.split()[1])
-        cand_dec = float(input_line.split()[2])
-
         if config['cutout_data']:
+            cand_ra = float(input_line.split()[1])
+            cand_dec = float(input_line.split()[2])
+
             data_tools.fetch_data((cand_ra, cand_dec), outname=name, bands=config['allbands'], outdir=config['datadir'])
 
         cand = yo.Candidate(name=name, bands=allbands, config=config)
